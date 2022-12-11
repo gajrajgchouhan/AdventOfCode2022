014c3657b401cba014cc5c8c0d9b34d47cb4adb8
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2022-12-11 11:40:58 +0530

    Added parts to days.

--- util/prepare_day.dart
+++ util/prepare_day.dart
@@ -1,41 +1,46 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 const year = '2022';
 
-void main(List<String> args) {
-  if (args.length != 2) {
-    print('Please call with: <dayNumber> <dayTitle>');
+void main(List<String> args) async {
+  if (args.length != 1) {
+    print('Please call with: <dayNumber>');
     return;
   }
 
   final dayNumber = args[0];
   final paddedDayNumber = dayNumber.padLeft(2, '0');
-  final dayTitle = args[1];
 
-  // Create lib file
-  final dayFileName = 'day$paddedDayNumber.dart';
+  final data = await http
+      .get(Uri.parse('https://adventofcode.com/$year/day/$dayNumber/input'));
 
-  File('lib/$dayFileName').writeAsString(
-    '''
-// --- Day $dayNumber: $dayTitle ---
+  // Create empty test data file
+  final dataPath = 'test/data/day$paddedDayNumber.txt';
+  File(dataPath).writeAsStringSync(data.body);
+
+  for (int i = 1; i <= 2; i++) {
+    final partNumber = i.toString().padLeft(2, '0');
+    // Create lib file
+    final dayFileName = 'day${paddedDayNumber}_part$partNumber.dart';
+
+    File('lib/$dayFileName').writeAsStringSync(
+      '''
+// --- Day $dayNumber: $partNumber ---
 // https://adventofcode.com/$year/day/$dayNumber
 
 int solveA(Iterable<String> input) {
   return 0;
 }
-''',
-  );
+    ''',
+    );
 
-  // Create empty test data file
-  final dataPath = 'test/data/day$paddedDayNumber.txt';
-  File(dataPath).create();
-
-  // Create unit tests
-  File(
-    'test/day${paddedDayNumber}_test.dart',
-  ).writeAsString(
-    '''
-// --- Day $dayNumber: $dayTitle ---
+    // Create unit tests
+    File(
+      'test/day${paddedDayNumber}_part${partNumber}_test.dart',
+    ).writeAsString(
+      '''
+// --- Day $dayNumber: Part $partNumber ---
 // https://adventofcode.com/$year/day/$dayNumber
 
 import 'dart:io';
@@ -46,18 +51,12 @@ final input = File('$dataPath').readAsLinesSync();
 
 void main() {
   group('Part One', () {
-    test('Example 1', () {
-      expect(
-          solveA(const [
-            '<someLine>',
-          ]),
-          equals(-1));
-    });
     test('Solution', () {
       expect(solveA(input), equals(-1));
     });
   });
 }
-''',
-  );
+    ''',
+    );
+  }
 }
